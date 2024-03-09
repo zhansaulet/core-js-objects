8e8ef19e87fa4b19ac73a89a0e50472532883bb8
Zhansaule Telisheva <jansaule97@mail.ru>
2024-03-10 00:08:53 +0500

    feat: add solutions of compareObjects and isEmptyObject tasks solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,6 +33,8 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
+// const result = objects.reduce((acc, curr) => Object.assign(acc, curr), {});
+
 function mergeObjects(/* objects */) {
   throw new Error('Not implemented');
 }
@@ -66,8 +68,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -81,8 +83,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
 }
 
 /**
