c34d0233ac45979d5138b605e5bd739662c10916
Zhansaule Telisheva <jansaule97@mail.ru>
2024-03-10 19:22:17 +0500

    feat: add a solution of sortCitiesArray task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -220,8 +220,26 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  const sortedArr = arr.sort(function sortFunc(a, b) {
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.country === b.country) {
+      if (a.city > b.city) {
+        return 1;
+      }
+      if (a.city < b.city) {
+        return -1;
+      }
+      return 0;
+    }
+    return 0;
+  });
+  return sortedArr;
 }
 
 /**
